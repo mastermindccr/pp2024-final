9f47aa9f39b937bdfac70c06a11fff409719614e
楊宇盛 <ysyang@ysyangs-MacBook-Air.local>
2024-12-22 23:34:20 +0800

    Add SIMD

---
+++ src/SIMD/README.md
@@ -0,0 +1,6 @@
+# SIMD
+## Useage
+1. `make` to build
+2. `./main_SIMD5 <HASH>` to run target size 5 or `./main_SIMD6 <HASH>` to run with target size 6
+
+HASH5=44a2b258d962b934c9f4d54b4460160d
---
+++ src/SIMD/main_SIMD5.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/time.h>
+
+#include "md5.h"
+
+#define BATCH_SIZE 4
+
+char charset[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+int target_size = 5;
+
+char* generate_string(int number) {
+    char* result = (char*) malloc(target_size+1);
+    result[target_size] = '\0';
+    for(int i = 0;i<target_size;i++) {
+        result[target_size-i-1] = charset[number % 62];
+        number /= 62;
+    }
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+    if(argc != 2){
+        printf("Usage: %s <hash>\n", argv[0]);
+        return 1;
+    }
+    uint8_t result[16];
+    uint8_t target[16];
+    for(int i = 0;i<16;i++) {
+        sscanf(argv[1] + 2*i, "%2hhx", target + i);
+    }
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    //---------------修改main function的部分-----------------
+    char* strings[BATCH_SIZE];
+    uint8_t* results[BATCH_SIZE];
+    uint8_t result_buffers[BATCH_SIZE][16];
+    
+    // 設置results指針陣列
+    for(int i = 0; i < BATCH_SIZE; i++) {
+        results[i] = result_buffers[i];
+    }
+    for(int i = 0; i<916,132,832; i+=BATCH_SIZE) { // enumerate all possible strings
+        
+        // 生成一批字串
+        for(int j = 0; j < BATCH_SIZE; j++) {
+            strings[j] = generate_string(i + j);
+        }
+    
+        // 並行計算MD5
+        md5String_SIMD(strings, results);
+    
+        // 檢查結果
+        for(int j = 0; j < BATCH_SIZE; j++) {
+            if(!memcmp(results[j], target, 16)){
+                printf("Found: %s\n", strings[j]);
+                goto found;
+            }
+            free(strings[j]);
+        }
+    }
+found:
+    //--------------------------------
+    gettimeofday(&end, NULL);
+    printf("Time: %f s\n", (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0);
+}
